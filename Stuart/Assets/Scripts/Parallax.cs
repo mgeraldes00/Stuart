5d0f3322fe1ba566e59bb462d126a10148000e60
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-31 11:25:20 +0100

    Add parallax effect

---
+++ Stuart/Assets/Scripts/Parallax.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private Transform sourceTransform;
+    [SerializeField] private Vector2 moveScale = Vector2.one;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(
+            transform.position.x - sourceTransform.position.x * moveScale.x,
+            transform.position.y - sourceTransform.position.y * moveScale.y,
+            transform.position.z - sourceTransform.position.z);
+    }
+
+    void Update()
+    {
+        Vector3 currentPos = new Vector3();
+        currentPos.x = sourceTransform.position.x * moveScale.x + offset.x;
+        currentPos.y = sourceTransform.position.y * moveScale.y + offset.y;
+        currentPos.z = sourceTransform.position.z + offset.z;
+        transform.position = currentPos;
+    }
+}
